a7e18f0a5cd1bd253dfe9884d543871ededcf610
oggii <24295020+oggii@users.noreply.github.com>
2026-03-26 05:45:08 +0100

    Optimization: AVIF format configuration for Next/Image

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     compress: true,
     images: {
+        formats: ['image/avif', 'image/webp'],
         remotePatterns: [
             {
                 protocol: 'https',

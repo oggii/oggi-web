ffb8f38fb7fd2d86c035ebf86cd8c0cfa42fc6bf
oggii <24295020+oggii@users.noreply.github.com>
2026-04-07 01:16:44 +0000

    Fix iconify import in portfolio detail page

--- src/app/[locale]/portfolio/[slug]/page.tsx
+++ src/app/[locale]/portfolio/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from 'next/navigation';
-import { Icon } from '@iconify/react/dist/iconify.server';
+import { Icon } from '@iconify/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import FooterSection from '@/components/sections/FooterSection';
